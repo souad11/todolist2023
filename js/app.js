6a1b0bd283449dfe9e58abed54d9a3058d784f64
Souad Riouch <riouch.souad@gmail.com>
2023-03-23 12:21:15 +0100

    Version 0.1 fonctionnel

--- js/app.js
+++ js/app.js
@@ -3,3 +3,21 @@
 const inputTask = document.forms['frmTODO'].task;
 const btAdd = document.forms['frmTODO'].btAdd;
 const taskList = document.getElementById('taskList');
+const divNotification = document.getElementById('notification');
+
+//Traitements des actions
+btAdd.onclick = function() {
+	//Lecture des données
+	let task = inputTask.value;
+	
+	//Validation des données
+	if(task!='' && task.length>=3) {
+		divNotification.innerHTML = '';
+		//Traitements des sonnées
+		taskList.innerHTML += '<li>'+task+'</li>';
+	} else {
+		//Notification des erreurs
+		divNotification.innerHTML = 'Veuillez entrer une tâche d\'au moins 3 caractères.';
+	}
+	
+}
