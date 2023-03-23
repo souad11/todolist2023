ac513cb8666911c71ef746e7669b78697ca4d363
Souad Riouch <riouch.souad@gmail.com>
2023-03-23 11:56:13 +0100

    Ajout du script JS

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/* JS */
+//Séléction des éléments HTML
+const inputTask = document.forms['frmTODO'].task;
+const btAdd = document.forms['frmTODO'].btAdd;
+const taskList = document.getElementById('taskList');
